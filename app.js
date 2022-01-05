e4e84d5b8f923e954a306c3803e345d6fe7e2ada
Natalia Fedorova <nataliafedorova1989@gmail.com>
2022-01-05 15:11:09 +0100

    define and initialize the movie model

---
+++ app.js
@@ -0,0 +1,31 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "movies.db",
+});
+
+// Movie model
+class Movie extends Sequelize.Model {}
+Movie.init(
+  {
+    title: Sequelize.STRING,
+  },
+  { sequelize } // same as { sequelize: sequelize }
+);
+
+// async IIFE
+(async () => {
+  // Sync all tables
+  await sequelize.sync({ force: true });
+
+  try {
+    // Instance of the Movie class represents a database row
+    const movie = await Movie.create({
+      title: "Toy Story",
+    });
+    console.log(movie.toJSON());
+  } catch (error) {
+    console.error("Error connecting to the database: ", error);
+  }
+})();
